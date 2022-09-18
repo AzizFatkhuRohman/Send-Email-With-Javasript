e97b97b01eda339108bb822e8a2c560c3b2f2433
AzizFatkhuRohman <aziz.rohman.stmik@krw.horizon.ac.id>
2022-09-19 01:51:50 +0700

    40

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -68,14 +68,18 @@ function renderBlog() {
                 </div>
                 <div class="ContentProject">
                     <a href="projectDetail.html" target="_blank" class="TitleProject">${dataBlog[index].title}</a>
-                    <p class="DistanceProject"> ${getFullTime(dataBlog[index].postAt)} | ${dataBlog[index].author}</p>
+                    <p class="DistanceProject">${dataBlog[index].postAt} | ${dataBlog[index].author}</p>
                     <p class="DescriptionProject">${dataBlog[index].description}</p>
                     <div class="ProjectCardIcon">
                         <div class="ItemIconProject">
-                            <img src="${dataBlog[index].MobileApp}" alt="phone">
-                            <img src="${dataBlog[index].WebApp}" alt="web">
+                        <i class="fa-brands fa fa-android${dataBlog[index].Mobileapp}"></i>
+                        <i class="fa-brands fa fa-stack-overflow${dataBlog[index].WebApp}"></i>
+                        <i class="fa-brands ffa fa-windows${dataBlog[index].DesktopApp}"></i>
                         </div>
                     </div>
+                    <div class="DistanceProject">
+                        <p>${getDistanceTime(dataBlog[index].postAt)}</p>
+                      </div>
                     <div class="ProjectAction">
                         <a href="#" class="Edit"><button class="Edit">Edit</button></a>
                         <a href="#" class="Delete"><button class="Delete">Delete</button></a>
@@ -106,4 +110,34 @@ function getFullTime(time) {
   let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`;
 
   return fullTime;
+}
+function getDistanceTime(time){
+
+  let timeNow = new Date()
+  let timePost = time
+
+ 
+  let distance = timeNow - timePost
+  console.log(distance);
+
+  let milisecond = 1000
+  let secondInHours = 3600
+  let hoursInDay = 24
+
+  let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
+  let distanceHours = Math.floor(distance / (milisecond * 60 * 60))
+  let distanceMinutes = Math.floor(distance / (milisecond * 60))
+  let distanceSecond = Math.floor(distance / milisecond)
+  
+  
+  if(distanceDay > 0){
+      return `${distanceDay} day ago`
+  } else if(distanceHours > 0){
+      return `${distanceHours} hours ago`
+  } else if(distanceMinutes > 0){
+      return `${distanceMinutes} minutes ago`
+  } else {
+      return `${distanceSecond} seconds ago`
+  }
+  
 }
